4cb56a9d5192eeb587832a7af5e1b0f989f6ed54
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-26 19:09:02 +0700

    halaman edit

--- app/src/main/java/com/example/questapi_152/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_152/view/HalamanEdit.kt
@@ -1,5 +1,38 @@
-package com.example.questapi_152.view
+package com.example.localrestapi.view
 
-class HalamanEdit {
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+
+        val coroutineScope = rememberCoroutineScope()
+
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
-//
