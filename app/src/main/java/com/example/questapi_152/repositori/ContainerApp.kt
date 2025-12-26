7ed766fada473f0ffd9c25d0d1817c3033851a96
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-26 18:51:05 +0700

    default container

--- app/src/main/java/com/example/questapi_152/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_152/repositori/ContainerApp.kt
@@ -14,3 +14,11 @@ interface ContainerApp{
     val repositoryDataSiswa: RepositoryDataSiswa
 }
 
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level= HttpLoggingInterceptor.Level.BODY
+
+    }
+
