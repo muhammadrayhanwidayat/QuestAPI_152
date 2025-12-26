a92281707374cb0f07d40efd7cb9d0e22a141bbc
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-26 18:43:59 +0700

    pembaruan

--- app/src/main/java/com/example/questapi_152/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_152/apiservice/ServiceApiSiswa.kt
@@ -1,15 +1,17 @@
-package com.example.questapi_152.apiservice
+package com.example.localrestapi.apiservice
 
-import com.example.questapi_152.modeldata.DataSiswa
+import com.example.localrestapi.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
-interface ServiceApiSiswa{
+interface ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
 
     @POST("insertTM.php")
-    suspend fun postSiswa(@Body data: DataSiswa):retrofit2.Response<Void>
+    suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
-}
