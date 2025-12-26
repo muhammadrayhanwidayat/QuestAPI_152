88850016356c65c3ae0705e33d786b88a872624b
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-26 18:58:37 +0700

    navhost

--- app/src/main/java/com/example/questapi_152/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_152/uicontroller/PetaNavigasi.kt
@@ -27,3 +27,7 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
