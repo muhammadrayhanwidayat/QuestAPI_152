4a8655d4892ddcae7a8b1293354b995aa9e23092
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-26 18:55:44 +0700

    destinasi detail

--- app/src/main/java/com/example/questapi_152/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_152/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
-package com.example.questapi_152.uicontroller.route
+package com.example.localrestapi.uicontroller.route
 
+
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
