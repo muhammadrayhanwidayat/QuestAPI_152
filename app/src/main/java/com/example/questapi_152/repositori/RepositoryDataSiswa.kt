dc4037a8a8e1af4c4c03b379ed8c0a4388911894
muhammadrayhanwidayat <murayat99@gmail.com>
2025-12-26 18:52:52 +0700

    pembaruan

--- app/src/main/java/com/example/questapi_152/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_152/repositori/RepositoryDataSiswa.kt
@@ -1,17 +1,13 @@
-package com.example.questapi_152.repositori
+package com.example.localrestapi.repositori
 
-import com.example.questapi_152.apiservice.ServiceApiSiswa
-import com.example.questapi_152.modeldata.DataSiswa
+import com.example.localrestapi.apiservice.ServiceApiSiswa
+import com.example.localrestapi.modeldata.DataSiswa
 
-interface RepositoryDataSiswa{
+interface RepositoryDataSiswa {
     suspend fun getDataSiswa(): List<DataSiswa>
-
-    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun getSatuSiswa(id: Int): DataSiswa
+    suspend fun editSatuSiswa(id: Int, dataSiswa: DataSiswa): retrofit2.Response<Void>
+    suspend fun hapusSatuSiswa(id: Int): retrofit2.Response<Void>
 }
 
-class JaringanRepositoryDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa{
-    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
-}
